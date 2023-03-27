7e15ab231da75c8279ce05d467f5b5d4b039e920
Youngheon Choi <102468071+eEonTown@users.noreply.github.com>
2023-03-26 21:44:39 -0700

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,22 @@
 "use strict";
 
-console.log("Hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+  psword = document.querySelector("#psword"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.value,
+        psword : psword.value
+    }
+
+    fetch("/login", {
+        method : "POST",
+        header : {
+            "Content-Type" : "application/json",
+        },
+        body : JSON.stringify(req)
+    })
+}
