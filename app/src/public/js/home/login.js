a45a71a3c97184d09feacb619be1e0e3c81be34b
Youngheon Choi <102468071+eEonTown@users.noreply.github.com>
2023-03-27 19:59:41 -0700

    프론트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -23,6 +23,13 @@ function login(){
         return res.json();
     })
     .then((res) => {
-        console.log(res);
+        if(res.success){
+            location.href = "/";
+        }else{
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error("로그인 중 에러 발생");
     })
 }
